43ed37fc87f51766741a0cd636a7a81d339cd0d0
QuadNard <lizardkingnardo@gmail.com>
2022-12-04 22:50:48 -0500

    application

---
+++ app/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Header() {
+  return <header className='text-4xl text-center css_text'>   I am Header</header>;
+}
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Home() {
+  return  <div className='text-4xl text-center css_text'>Home</div>
+}
+export default Home;
